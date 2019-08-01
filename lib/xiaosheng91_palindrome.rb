3d8417209728fd7e07a994657fe46a75ffd94eec
Huang Xiaosheng <xiaosheng91@outlook.com>
2019-08-01 22:02:26 +0800

    Support integer

--- lib/xiaosheng91_palindrome.rb
+++ lib/xiaosheng91_palindrome.rb
@@ -1,16 +1,28 @@
 require "xiaosheng91_palindrome/version"
 
-class String
+module Xiaosheng91Palindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/\w/).join.downcase
     end
+end
+
+class String
+  include Xiaosheng91Palindrome
+end
+
+class Integer
+  include Xiaosheng91Palindrome
 end
